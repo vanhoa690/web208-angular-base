f53af3f377b13895d75b4f5124e775549aff19c5
hoadv <vanhoa690@gmail.com>
2025-08-07 10:41:06 +0700

    add ui and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { List } from './list/list';
+import { Add } from './add/add';
+import { Update } from './update/update';
+import { Register } from './register/register';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: List,
+  },
+  {
+    path: 'add',
+    component: Add,
+  },
+  {
+    path: 'update/:id',
+    component: Update,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+];
