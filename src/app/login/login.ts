4fdf99cefc1e17abde5afe479c4e0b5e1b0d67d2
hoadv <vanhoa690@gmail.com>
2025-08-07 11:15:04 +0700

    add register login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      email: [''],
+      password: [''],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    this.authService.loginUser(this.loginForm.value).subscribe({
+      next: () => {},
+    });
+  }
 }
--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ProductService } from '../product.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.scss'
+  styleUrl: './register.scss',
 })
 export class Register {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      email: [''],
+      password: [''],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+    this.authService.registerUser(this.registerForm.value).subscribe({
+      next: () => {},
+    });
+  }
 }
